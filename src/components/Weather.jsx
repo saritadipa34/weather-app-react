6804bcecec42d8b95f61d66dc3947b0ba2f0c265
saritadipa34 <saritadipa34@gmail.com>
2025-07-19 14:05:04 +0530

    Search field UI created

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,8 +1,14 @@
+import { CiSearch } from "react-icons/ci";
 
 const Weather=()=>{
     return(
-        <div className="text-green-400">
-            Weather
+        <div className="h-[500px] w-[450px] bg-blue-300 px-10 py-15">
+            <div className="flex justify-between">
+                <input type="text" className="h-[50px] w-3/4 border-1 rounded-3xl" />
+                <div className="h-[50px] w-[60px] border-1 rounded-full flex items-center justify-center">
+                <CiSearch />
+                </div>
+            </div>
         </div>
     )
 }
